ecf4dd0eea88c87cb21c3705da97cac069c58d40
Priyanshuprinci <princi.it.js@gmail.com>
2025-03-16 11:46:36 +0530

    issue solved

--- config/db.js
+++ config/db.js
@@ -1,21 +1,15 @@
 
-const dotenv = require('dotenv');
+require('dotenv').config();
 
-
-dotenv.config();
 
 
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const mongoURI = process.env.MONGO_URI;
-
-    if (!mongoURI) {
-      throw new Error("MONGO_URI is not defined in environment variables");
-    }
     
-    const conn = await mongoose.connect(mongoURI, {
+    
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
