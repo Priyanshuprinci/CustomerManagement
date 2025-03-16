146f3377043ae8a9a127fcdd09113914c013b182
Priyanshuprinci <princi.it.js@gmail.com>
2025-03-16 16:34:39 +0530

    Updated code

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(express.json());
 
 app.use("/api", customerRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 9000;
 
 const server = app.listen(PORT, () =>
   console.log(`🚀 Server running on port ${PORT}`)
