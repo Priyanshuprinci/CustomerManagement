cbe9801269d3d75f5814c60bca6d04260d256292
Priyanshuprinci <princi.it.js@gmail.com>
2025-03-16 08:34:34 +0530

    issue solved

--- config/db.js
+++ config/db.js
@@ -1,21 +1,45 @@
+// const mongoose = require('mongoose');
+// const dotenv = require('dotenv');
+
+
+// dotenv.config();
+
+// const connectDB = async () => {
+//   try {
+//     const conn = await mongoose.connect(process.env.MONGO_URI, {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true,
+      
+//     });
+//     console.log(`MongoDB Connected: ${conn.connection.host}`);
+//   } catch (error) {
+//     console.error(`Error: ${error.message}`);
+//     process.exit(1);
+//   }
+// };
+// module.exports = connectDB;
+
 const mongoose = require('mongoose');
-const dotenv = require('dotenv');
-
-
-dotenv.config();
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
+    const mongoURI = process.env.MONGO_URI;
+
+    if (!mongoURI) {
+      throw new Error("MONGO_URI is not defined in environment variables");
+    }
+
+    const conn = await mongoose.connect(mongoURI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
-      
     });
+
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
-    process.exit(1);
+    process.exit(1); // Stop the app if MongoDB fails to connect
   }
 };
+
 module.exports = connectDB;
 
